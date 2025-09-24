6c9d816c6f693c542ceac6c23a09bb82276ff876
YangYounghwa <yh9369@gmail.com>
2025-09-24 11:55:24 +0900

    getPurpose  method added and tested.
    (actually means type, but
    Emuseum API gets parameter name as 'purpose' but their service list them as types.

--- flask_app/services/LLMService/LLMServiceObject.py
+++ flask_app/services/LLMService/LLMServiceObject.py
@@ -14,7 +14,7 @@ from langchain.output_parsers import PydanticOutputParser
 from langchain_core.exceptions import OutputParserException 
 from langchain.prompts import PromptTemplate
 
-from flask_app.dto.llmserviceDTO import KorRelation, KoreanNationsEnum, Material_descriptions, Meterial, Nations, K_nation_descriptions
+from flask_app.dto.llmserviceDTO import KorRelation, KoreanNationsEnum, Material_descriptions, Meterial, Nations, K_nation_descriptions, Purpose, Purpose_descriptions
 from langchain_community.callbacks import get_openai_callback
 from dotenv import load_dotenv
 load_dotenv()
@@ -166,3 +166,51 @@ Your response must be a single item from the list. Follow the specified format i
         current_app.logger.info(f"Completion Tokens: {cb.completion_tokens}")
         
         return material
+    
+    
+    
+    
+    def getPurpose(self, text:str)->Purpose:
+        Purpose_descriptions.purpose_descriptions
+        
+        # Step 3: Create an instance of the PydanticOutputParser
+        parser = PydanticOutputParser(pydantic_object=Purpose)
+        template="""글을 읽고 해당 유물 혹은 글 내용에서 유추할 수 있는 주 용도를 알려줘. 가장 간단한 기능을 우선하고 부차적인 것은 무시해.
+        Here are the available options with brief descriptions: {options_with_descriptions}.
+
+Your response must be a single item from the list. Follow the specified format instructions below.
+        {format_instructions}
+        Text: {text}
+        """
+        options_with_descriptions = "\n".join([
+    f"* **{pur}**: {desc}" for pur, desc in Purpose_descriptions.purpose_descriptions.items()
+])
+
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["text"],
+            partial_variables={
+                "options_with_descriptions": options_with_descriptions,
+                "format_instructions": parser.get_format_instructions()
+            }
+        )
+        llm = self.llm_nano_cold
+        # llm = self.llm_mini_cold
+        chain = prompt | llm | parser
+        
+        with get_openai_callback() as cb:
+            try:
+                purpose = chain.invoke({"text": text})
+                current_app.logger.debug(f"Purpose :{purpose.name}")
+                
+            except OutputParserException as e:
+                # TODO : priority low,  add fallback.
+                current_app.logger.debug(f"Failed to parse LLM response")
+                current_app.logger.debug(f"Arguments {e.args[0]}")
+                relation = {"error": True}
+        # Access the token counts after the LLM call
+        current_app.logger.info(f"Total Tokens: {cb.total_tokens}")
+        current_app.logger.info(f"Prompt Tokens: {cb.prompt_tokens}")
+        current_app.logger.info(f"Completion Tokens: {cb.completion_tokens}")
+        
+        return purpose
